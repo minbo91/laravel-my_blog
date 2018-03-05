5b66122e933dbd7e0d57e68843426b507d75e3c5
minbo <837115102@qq.com>
2018-03-05 15:35:57 +0800

    修改配置文件

--- database/seeds/LinksTableSeeder.php
+++ database/seeds/LinksTableSeeder.php
@@ -13,17 +13,7 @@ class LinksTableSeeder extends Seeder
     {
         $data = [
             [
-                'link_name' => '后盾网',
-                'link_title' => '国内其中一个培训机构',
-                'link_url' => 'http://www.houdunwang.com',
-                'link_order' => 1,
-            ],
-            [
-                'link_name' => '后盾网论坛',
-                'link_title' => '培训机构的论坛',
-                'link_url' => 'http://bbs.houdunwang.com',
-                'link_order' => 2,
-            ],
+          
         ];
         DB::table('links')->insert( $data );
     }
